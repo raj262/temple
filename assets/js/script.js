f460904197182d2375fec5f7c38d4fbe3a2aab64
Rajesh <rajeshverma@Rajeshs-MacBook-Air.local>
2025-05-04 21:35:33 +0530

    home page

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    AOS.init({
+      duration: 1000,   // duration in milliseconds
+      once: true        // animate only once when scrolled into view
+    });
+  });
