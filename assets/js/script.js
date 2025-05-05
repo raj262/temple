39c2bea8e6d03f220a7250353e5ab2f65a377753
Rajesh <rajeshverma@Rajeshs-MacBook-Air.local>
2025-05-05 10:01:07 +0530

    new changes

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    AOS.init({
-      duration: 1000,   // duration in milliseconds
-      once: true        // animate only once when scrolled into view
-    });
-  });
