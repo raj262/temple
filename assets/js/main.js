0a1a61a5b024d002cb6b55465710517f49dc2200
Rajesh <rajeshverma@Rajeshs-MacBook-Air.local>
2025-05-08 00:14:50 +0530

    new changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -986,10 +986,10 @@
 })(jQuery);
 
  /*----------- 00. Right Click Disable ----------*/
-  window.addEventListener('contextmenu', function (e) {
-    // do something here... 
-    e.preventDefault();
-  }, false);
+  // window.addEventListener('contextmenu', function (e) {
+  //   // do something here... 
+  //   e.preventDefault();
+  // }, false);
 
 
   /*----------- 00. Inspect Element Disable ----------*/
